97cb80f63fa789dfd959b660d60658f916660b28
Mike <wong.michael.shane@gmail.com>
2025-06-05 13:36:49 -0700

    added request context for authentication authorization

--- internal/models/users.go
+++ internal/models/users.go
@@ -79,5 +79,12 @@ func (m *UserModel) Authenticate(email, password string) (int, error) {
 }
 
 func (m *UserModel) Exists(id int) (bool, error) {
-	return false, nil
+	var exists bool
+
+	stmt := `
+	SELECT EXISTS(SELECT 1 FROM users WHERE id = $1)
+	`
+
+	err := m.Pool.QueryRow(context.Background(), stmt, id).Scan(&exists)
+	return exists, err
 }
